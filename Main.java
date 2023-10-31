32b7188331fe9425954007cc1f2d39368460f858
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-10-31 14:00:11 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package Porta;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Porta p1 = new Porta();
+		
+		p1.abre();
+		p1.estaAberta();
+		p1.fechar();
+		p1.estaAberta();
+		p1.pintar("vermelho");
+
+	}
+
+}

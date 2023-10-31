912663620f8d102a39b5e9e35e832947b289cf5f
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-10-31 14:12:17 -0300

    Update Casa.java

--- Casa.java
+++ Casa.java
@@ -3,5 +3,13 @@ package Porta;
 public class Casa {
 
 	String cor;
+	int qntdPortas = 4;
+	
+	public void pintar(String cor) {
+		
+		this.cor = cor;
+		System.out.println("Pronto, agora a casa é " + cor);
+	}
+	
 	
 }

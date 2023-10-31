126ff36f8ca72ddc11b60d16ea0558b893380b66
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-10-31 14:38:15 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,12 +6,24 @@ public class Main {
 	
 		Porta p1 = new Porta();
 		
+		//Questão porta 
+		
 		p1.abre();
 		p1.estaAberta();
 		p1.fechar();
 		p1.estaAberta();
 		p1.pintar("vermelho");
+		
+		//Questão casa
 
+		Casa c1 = new Casa();
+		c1.pintar("Azul");
+		c1.abrir();
+		c1.fechar();
+		c1.abrir();
+		c1.abrir();
+		c1.imprimirPortas();
+		
 	}
 
 }

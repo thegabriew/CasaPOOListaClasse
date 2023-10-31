63591c91d830931a9ff20b41ea69a1c4dcf6e616
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-10-31 14:30:51 -0300

    Update Casa.java

--- Casa.java
+++ Casa.java
@@ -1,9 +1,12 @@
 package Porta;
+import java.util.Scanner;
 
 public class Casa {
 
 	String cor;
-	int qntdPortas = 4;
+	int porta, qntdPortas;
+
+	Scanner scan = new Scanner(System.in);
 	
 	public void pintar(String cor) {
 		
@@ -11,5 +14,38 @@ public class Casa {
 		System.out.println("Pronto, agora a casa é " + cor);
 	}
 	
+	public void abrir() {
+		System.out.println("Que porta deseja abrir?");
+		porta = scan.nextInt();
+		if(porta ==1) {
+			System.out.println("A porta 1 foi aberta");
+		}
+		else if(porta==2) {
+			System.out.println("A porta 2 foi aberta");
+		} else if(porta==3) {
+			System.out.println("A porta 3 foi aberta");
+		}else {
+			System.out.println("Valor inválido");
+		}
+	}
+	
+	public void fechar() {
+		System.out.println("Que porta deseja fechar?");
+		porta = scan.nextInt();
+		if(porta ==1) {
+			System.out.println("A porta 1 foi fechada");
+		}
+		else if(porta==2) {
+			System.out.println("A porta 2 foi fechada");
+		} else if(porta==3) {
+			System.out.println("A porta 3 foi fechada");
+		}else {
+			System.out.println("Valor inválido");
+		}
+	}
+	
+	public void imprimirPortas() {
+		System.out.println("Existem 3 portas.");
+	}
 	
 }
